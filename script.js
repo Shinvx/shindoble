97d28e330cd046edeb8e9f2217b3c74cb20fa1e0
Shin <156226474+Shinvx@users.noreply.github.com>
2025-03-07 20:49:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 const word = "SHINDOBLE";
 const container = document.getElementById("shindoble");
-const letterSpacing = 6; 
+const letterSpacing = 6;
 
 word.split('').forEach((letter, index) => {
     const span = document.createElement('span');
     span.textContent = letter;
     span.classList.add('letter');
     
-
-    const randomX = Math.random() * 100; 
+    const randomX = Math.random() * 100;
     const randomY = Math.random() * 100;
     span.style.transform = `translate(${randomX}vw, ${randomY}vh)`;
     
     container.appendChild(span);
     
-   
-    const finalX = (index - (word.length - 1) / 2) * letterSpacing; 
-    const finalY = 0; 
-    
+    const finalX = (index - (word.length - 1) / 2) * letterSpacing;
+    const finalY = 0;
     
     setTimeout(() => {
         span.style.opacity = 1;
-        span.style.transform = `translate(${finalX}rem, ${finalY}rem)`; 
-    }, index * 200); 
+        span.style.transform = `translate(${finalX}rem, ${finalY}rem)`;
+    }, index * 200);
 });
